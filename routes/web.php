5451dc42ddfb47dd242328f7f4f4be5b495563ed
david <telegd.david12@gmail.com>
2024-06-12 09:08:48 +0300

    Socialite google driver integrated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('auth/google/callback', function () {
+    $user = Socialite::driver('google')->user();
+
+    dd($user);
 });
